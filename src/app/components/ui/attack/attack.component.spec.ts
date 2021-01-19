23cbe31ebe19cdeeda56204e15bbc6d365f9fd8a
RoyALeeST <rodrigo.villalobos.sab@gmail.com>
2021-01-18 19:00:54 -0600

    test files for new components

--- src/app/components/ui/attack/attack.component.spec.ts
+++ src/app/components/ui/attack/attack.component.spec.ts
@@ -18,8 +18,4 @@ describe('AttackComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
